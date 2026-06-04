fed8a4fd5ab547225d1604a8092380bb37265c5e
yoran dekoker <yoradeko@student.arteveldehs.be>
2026-06-04 22:39:12 +0200

    fix vercel routing

--- server/app.ts
+++ server/app.ts
@@ -25,6 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use((req, res, next) => { res.setHeader('Cache-Control', 'no-store'); next(); });
 app.use(cookieParser());
